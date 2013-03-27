0a81e21d911d011711015c003db03a30504849fc
Hatindera Singh <hatindera@Hatindera.(none)>
2013-03-27 10:34:50 +0530

    multiplication

---
+++ multiplication.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main (void)
+{
+int mytable;
+cout<<"\n\n please enter a number:";
+cin>>mytable;
+for (int i=0;i<=10'i++)
+{
+cout<<mytable<<\t *\t <<i+1
+<<"\t=\t"<<mytable*i+1<<"\n";
+}
+return 0;
+}
+
